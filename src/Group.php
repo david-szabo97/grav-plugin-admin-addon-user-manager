8448ec3948ae34895fbdfdc21e04a6c4221c2fb6
Dávid Szabó <david.szabo97@gmail.com>
2017-07-27 23:02:36 +0200

    Refactor to be consistent with the rest of the codebase

--- src/Group.php
+++ src/Group.php
@@ -9,144 +9,140 @@ use Grav\Common\File\CompiledYamlFile;
 use Grav\Common\Grav;
 use Grav\Common\Utils;
 
-class Group extends Data
-{
-    /**
-     * Get the groups list
-     *
-     * @return array
-     */
-    public static function groups()
-    {
-        $groups = Grav::instance()['config']->get('groups');
+class Group extends Data {
 
-        $blueprints = new Blueprints;
-        $blueprint = $blueprints->get('user/group');
-        foreach ($groups as $groupname => &$content) {
-            if (!isset($content['groupname'])) {
-                $content['groupname'] = $groupname;
-            }
-            $content = new Group($content, $blueprint);
+  /**
+   * Get the groups list
+   *
+   * @return array
+   */
+  public static function groups() {
+    $groups = Grav::instance()['config']->get('groups');
+
+    $blueprints = new Blueprints;
+    $blueprint = $blueprints->get('user/group');
+    foreach ($groups as $groupname => &$content) {
+      if (!isset($content['groupname'])) {
+        $content['groupname'] = $groupname;
+      }
+      $content = new Group($content, $blueprint);
+    }
+
+    return $groups;
+  }
+
+  /**
+   * Get the groups list
+   *
+   * @return array
+   */
+  public static function groupNames() {
+    $groups = [];
+
+    foreach(Grav::instance()['config']->get('groups', []) as $groupname => $group) {
+      $groups[$groupname] = isset($group['readableName']) ? $group['readableName'] : $groupname;
+    }
+
+    return $groups;
+  }
+
+  /**
+   * Checks if a group exists
+   *
+   * @param string $groupname
+   *
+   * @return bool
+   */
+  public static function groupExists($groupname) {
+    return isset(self::groups()[$groupname]);
+  }
+
+  /**
+   * Get a group by name
+   *
+   * @param string $groupname
+   *
+   * @return object
+   */
+  public static function load($groupname) {
+    if (self::groupExists($groupname)) {
+      $group = self::groups()[$groupname];
+    } else {
+      $blueprints = new Blueprints;
+      $blueprint = $blueprints->get('user/group');
+      $content = [];
+      $group = new Group($content, $blueprint);
+    }
+
+    return $group;
+  }
+
+  /**
+   * Save a group
+   */
+  public function save() {
+    $grav = Grav::instance();
+    $config = $grav['config'];
+
+    $blueprints = new Blueprints;
+    $blueprint = $blueprints->get('user/group');
+
+    $fields = $blueprint->fields();
+
+    $config->set("groups.$this->groupname", []);
+
+    foreach ($fields as $field) {
+      if ($field['type'] == 'text') {
+        $value = $field['name'];
+        if (isset($this->items[$value])) {
+          $config->set("groups.$this->groupname.$value", $this->items[$value]);
         }
+      }
 
-        return $groups;
-    }
+      if ($field['type'] == 'array' || $field['type'] == 'permissions') {
+        $value = $field['name'];
+        $arrayValues = Utils::getDotNotation($this->items, $field['name']);
 
-    /**
-     * Get the groups list
-     *
-     * @return array
-     */
-    public static function groupNames()
-    {
-        $groups = [];
-
-        foreach(Grav::instance()['config']->get('groups', []) as $groupname => $group) {
-            $groups[$groupname] = isset($group['readableName']) ? $group['readableName'] : $groupname;
+        if ($arrayValues) {
+          foreach ($arrayValues as $arrayIndex => $arrayValue) {
+            $config->set("groups.$this->groupname.$value.$arrayIndex", $arrayValue);
+          }
         }
-
-        return $groups;
+      }
     }
 
-    /**
-     * Checks if a group exists
-     *
-     * @param string $groupname
-     *
-     * @return bool
-     */
-    public static function groupExists($groupname)
-    {
-        return isset(self::groups()[$groupname]);
-    }
+    $type = 'groups';
+    $blueprints = $this->blueprints("config/{$type}");
+    $obj = new Data($config->get($type), $blueprints);
+    $file = CompiledYamlFile::instance($grav['locator']->findResource("config://{$type}.yaml"));
+    $obj->file($file);
+    $obj->save();
+  }
 
-    /**
-     * Get a group by name
-     *
-     * @param string $groupname
-     *
-     * @return object
-     */
-    public static function load($groupname)
-    {
-        if (self::groupExists($groupname)) {
-            $group = self::groups()[$groupname];
-        } else {
-            $blueprints = new Blueprints;
-            $blueprint = $blueprints->get('user/group');
-            $content = [];
-            $group = new Group($content, $blueprint);
-        }
+  /**
+   * Remove a group
+   *
+   * @param string $groupname
+   *
+   * @return bool True if the action was performed
+   */
+  public static function remove($groupname) {
+    $grav = Grav::instance();
+    $config = $grav['config'];
+    $blueprints = new Blueprints;
+    $blueprint = $blueprints->get('user/group');
 
-        return $group;
-    }
+    $groups = $config->get("groups");
+    unset($groups[$groupname]);
+    $config->set("groups", $groups);
 
-    /**
-     * Save a group
-     */
-    public function save()
-    {
-        $grav = Grav::instance();
-        $config = $grav['config'];
+    $type = 'groups';
+    $obj = new Data($config->get($type), $blueprint);
+    $file = CompiledYamlFile::instance($grav['locator']->findResource("config://{$type}.yaml"));
+    $obj->file($file);
+    $obj->save();
 
-        $blueprints = new Blueprints;
-        $blueprint = $blueprints->get('user/group');
+    return true;
+  }
 
-        $fields = $blueprint->fields();
-
-        $config->set("groups.$this->groupname", []);
-
-        foreach ($fields as $field) {
-            if ($field['type'] == 'text') {
-                $value = $field['name'];
-                if (isset($this->items[$value])) {
-                    $config->set("groups.$this->groupname.$value", $this->items[$value]);
-                }
-            }
-            if ($field['type'] == 'array' || $field['type'] == 'permissions') {
-                $value = $field['name'];
-                $arrayValues = Utils::getDotNotation($this->items, $field['name']);
-
-                if ($arrayValues) {
-                    foreach ($arrayValues as $arrayIndex => $arrayValue) {
-                        $config->set("groups.$this->groupname.$value.$arrayIndex", $arrayValue);
-                    }
-                }
-            }
-        }
-
-        $type = 'groups';
-        $blueprints = $this->blueprints("config/{$type}");
-        $obj = new Data($config->get($type), $blueprints);
-        $file = CompiledYamlFile::instance($grav['locator']->findResource("config://{$type}.yaml"));
-        $obj->file($file);
-        $obj->save();
-    }
-
-    /**
-     * Remove a group
-     *
-     * @param string $groupname
-     *
-     * @return bool True if the action was performed
-     */
-    public static function remove($groupname)
-    {
-        $grav = Grav::instance();
-        $config = $grav['config'];
-        $blueprints = new Blueprints;
-        $blueprint = $blueprints->get('user/group');
-
-        $groups = $config->get("groups");
-        unset($groups[$groupname]);
-        $config->set("groups", $groups);
-
-        $type = 'groups';
-        $obj = new Data($config->get($type), $blueprint);
-        $file = CompiledYamlFile::instance($grav['locator']->findResource("config://{$type}.yaml"));
-        $obj->file($file);
-        $obj->save();
-
-        return true;
-    }
 }
