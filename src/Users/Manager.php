1bd9704d5f13025a1254a989387825142df5019c
David Szabo <david.szabo97@gmail.com>
2018-02-07 20:29:22 +0100

    Fix wrong redirection after deleting an user #4

--- src/Users/Manager.php
+++ src/Users/Manager.php
@@ -130,7 +130,7 @@ class Manager implements IManager, EventSubscriberInterface {
     if ($method === 'taskUserDelete') {
       $username = $this->grav['uri']->paths()[2];
       if ($this->removeUser($username)) {
-        $this->grav->redirect($this->getLocation());
+        $this->adminController->setRedirect($this->getLocation());
       }
     } elseif ($method === 'taskUserLoginAs') {
       $username = $this->grav['uri']->paths()[2];
